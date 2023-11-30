b6cc70359bf4942fef8496509e396dd4b161d3eb
Sergei_Aristov <maxmit83@gmail.com>
2023-11-30 20:10:07 +0500

    the function returns a string in capital letters the function converts the first character of the string to uppercase

--- str_func.py
+++ str_func.py
@@ -2,3 +2,9 @@ def func(list_name):
     """ Возвращает строку заглавными буквами 
     """
     return list_name.upper()
+
+
+def func_2(list_name):
+    """ Переводит первый символ строки в верхний регистр, а все остальные в нижний
+    """
+    return list_name.capitalize()
